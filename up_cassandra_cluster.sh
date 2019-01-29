b82acc1ffd35d5947b2b704e20eef17e9ef396e5
Y-Wakuta <prsprivatestock@i.softbank.jp>
2019-01-30 00:19:57 +0900

    #1 reduce steps for cluster

--- up_cassandra_cluster.sh
+++ up_cassandra_cluster.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -e 
+broadcast_addr=$1
+seed_addr=$2
+
+cp base_docker-compose.yml docker-compose.yml
+
+sed -i -e "s/BROADCAST_ADDRESS=badr/BROADCAST_ADDRESS=$broadcast_addr/g"  docker-compose.yml
+sed -i -e "s/SEEDS=sadr/SEEDS=$seed_addr/g"  docker-compose.yml
 
 docker-compose down
 docker-compose build --no-cache
